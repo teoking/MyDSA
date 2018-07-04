7e9388189845b46bb1a8532ee812fd49bc7f9b9d
teoking <yagatong@gmail.com>
2018-07-04 23:28:14 +0800

    Add No 43 - Add best code reference.

--- src/alg/leetcode/No43MultiplyStrings.java
+++ src/alg/leetcode/No43MultiplyStrings.java
@@ -5,7 +5,7 @@ public class No43MultiplyStrings {
     // Emulate two numbers multiply by hand.
     // time: O(n^2)
     // space: O(n)
-    // runtime:
+    // runtime: 28ms
     private static String multiply(String num1, String num2) {
         char[] arr1 = num1.toCharArray();
         char[] arr2 = num2.toCharArray();
@@ -33,7 +33,7 @@ public class No43MultiplyStrings {
                 sb.append(arr[i]);
             }
         }
-        return sb.toString();
+        return "".equals(sb.toString())  ? "0" : sb.toString();
     }
 
     private static char[] reverse(char[] chars) {
@@ -60,6 +60,35 @@ public class No43MultiplyStrings {
         }
 
         return chars;
+    }
+
+    // The follow code has best performance:
+    // from LeetCode.
+    public String multiply2(String num1, String num2) {
+        StringBuilder sb = new StringBuilder(num1);
+        char[] arr1 = sb.reverse().toString().toCharArray();
+        sb.setLength(0);
+        sb.append(num2);
+        char[] arr2 = sb.reverse().toString().toCharArray();
+        int[] res = new int[arr1.length + arr2.length + 1];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                res[i + j] += (arr1[i] - '0') * (arr2[j] - '0');
+            }
+        }
+        sb.setLength(0);
+        int carry = 0;
+        for (int i = 0; i < res.length; i++) {
+            res[i] += carry;
+            sb.append(res[i] % 10);
+            carry = res[i] / 10;
+        }
+        int j = sb.length() - 1;
+        while (j >= 0 && sb.charAt(j) == '0') {
+            sb.deleteCharAt(j--);
+        }
+        String ans = sb.reverse().toString();
+        return ans.length() == 0 ? new String("0") : ans;
     }
 
     public static void main(String[] args) {
